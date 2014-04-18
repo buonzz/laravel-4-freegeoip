678176121f12515a677372d77063b76bceb04232
paperdarwin <psmdarwin@gmail.com>
2014-04-18 16:56:02 +0800

    updated the unit tests

--- tests/Unit/Buonzz/GeoIP/GeoIPTest.php
+++ tests/Unit/Buonzz/GeoIP/GeoIPTest.php
@@ -8,7 +8,7 @@
 *
 *  @author yourname
 */
-class YourClassTest extends PHPUnit_Framework_TestCase{
+class GeoIPTest extends PHPUnit_Framework_TestCase{
 	
   /**
   * Just check if the YourClass has no syntax error 
@@ -18,22 +18,16 @@ class YourClassTest extends PHPUnit_Framework_TestCase{
   *
   */
   public function testIsThereAnySyntaxError(){
-	$var = new VendorName\PackageName\YourClass;
-	$this->assertTrue(is_object($var));
-	unset($var);
+  	$var = new Buonzz\GeoIP\GeoIP;
+  	$this->assertTrue(is_object($var));
+  	unset($var);
   }
   
-  /**
-  * Just check if the YourClass has no syntax error 
-  *
-  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
-  * any typo before you even use this library in a real project.
-  *
-  */
-  public function testMethod1(){
-	$var = new VendorName\PackageName\YourClass;
-	$this->assertTrue($var->method1("hey") == 'Hello World');
-	unset($var);
+  
+  public function testgetCountry(){
+  	$var = new Buonzz\GeoIP\GeoIP;
+  	$this->assertTrue($var->getCountry() == 'Philippines');
+  	unset($var);
   }
   
 }
