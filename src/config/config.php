72be58acb6c516f112ff5e07348ce90eb0820ae2
Gregory Shayko <greg@shaykos.com>
2014-11-15 23:02:18 -0500

    Added config for overrides

--- src/config/config.php
+++ src/config/config.php
@@ -6,4 +6,10 @@ return array(
 	
 	// Timeout when calling the API (in seconds)
 	'timeout' => 30,
+	
+	// IP address overrides. Defaults localhost to Google
+	'overrides' => array(
+	  '127.0.0.1' => '64.233.160.0',
+	  'localhost' => '64.233.160.0',
+	)
 );
